3e8bad8f9abad80466b4644bc348cc0e32fbfa0e
Thein Naing <theinnaing0908@gmail.com>
2023-06-10 20:57:17 +0800

    in Store.jsx map the productsArray with (product, index) arguments , immidiately return like below;  <Col align='center' key={index}>                 <h1>{product.title}</h1>               </Col>

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,19 +1,21 @@
 import { Row, Col} from 'react-bootstrap';
+import { productsArray } from '../helper/productsArray';
 
 const Store = () => {
   return (
     <>
      <h1 align="center" className="p-3">Welcome to the store!</h1>
             <Row xs={1} md={3} className="g-4">
-                  <Col align='center'>
-                    <h1>Product</h1>
-                  </Col>
-                  <Col align='center'>
-                    <h1>Product</h1>
-                  </Col>
+              {productsArray.map((product, index) => (
+                <Col align='center' key={index}>
+                <h1>{product.title}</h1>
+              </Col>
+              ))}
+
+
             </Row>
     </>
   )
 }
 
-export default Store
+export default Store;
