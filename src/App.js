f6fcc4ecf4c4373091b828db109d78496e770ae7
Thein Naing <theinnaing0908@gmail.com>
2023-06-10 19:48:18 +0800

    create routes in App.js :  <Routes>             <Route index element={<Store />} />             <Route path="success" element={<Success />} />             <Route path="cancel" element={<Cancel />} />           </Routes> and create this components files in pages folder inside src

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import './App.css';
 import NavbarComponent from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
+import { BrowserRoutr as Router, Routes, Route} from 'react-router-dom';
+import Cancel from './pages/Cancel';
+import Store from './pages/Store';
+import Success from './pages/Success';
 
 function App() {
   return (
-   <NavbarComponent>
+    <Container>
+      <NavbarComponent> </NavbarComponent>
+        <Router>
+          <Routes>
+            <Route index element={<Store />} />
+            <Route path="success" element={<Success />} />
+            <Route path="cancel" element={<Cancel />} />
+          </Routes>
+        </Router>
 
-   </NavbarComponent>
+
+   </Container>
 
 
 
