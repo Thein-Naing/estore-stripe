f579ad5608467171f3c44af96fa7df3bcae6b4aa
Thein Naing <theinnaing0908@gmail.com>
2023-06-11 11:42:57 +0800

    update src folder, all componets import/export and now update ProductCard.jsx to add product items to cart. Add <cardProvider> element to App.js to wrap all .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
+// import "./App.css";
+// import NavbarComponent from "./components/Navbar";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { Container } from "react-bootstrap";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Cancel from "./pages/Cancel";
+// import Store from "./pages/Store";
+// import Success from "./pages/Success";
+// import CartProvider from "CartContext";
+
+// function App() {
+//   return (
+//     <CartProvider>
+//       <Container>
+//         <NavbarComponent> </NavbarComponent>
+//         <Router>
+//           <Routes>
+//             <Route index element={<Store />} />
+//             <Route path="success" element={<Success />} />
+//             <Route path="cancel" element={<Cancel />} />
+//           </Routes>
+//         </Router>
+//       </Container>
+//     </CartProvider>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
-import NavbarComponent from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './components/Navbar';
 import { Container } from 'react-bootstrap';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Cancel from './pages/Cancel';
 import Store from './pages/Store';
 import Success from './pages/Success';
+import CartProvider from './CartContext';
+// localhost:3000 -> Home
+// localhost:3000/success -> Success
 
 function App() {
   return (
-    <Container>
-      <NavbarComponent> </NavbarComponent>
+    <CartProvider>
+      <Container>
+        <NavbarComponent></NavbarComponent>
         <Router>
           <Routes>
             <Route index element={<Store />} />
@@ -18,12 +51,8 @@ function App() {
             <Route path="cancel" element={<Cancel />} />
           </Routes>
         </Router>
-
-
-   </Container>
-
-
-
+      </Container>
+    </CartProvider>
   );
 }
 
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,12 @@
 import { Card, Button, Form, Row, Col} from 'react-bootstrap';
+import { CartContext } from '../CartContext';
+import { useContext } from 'react';
 
 const ProductCard = (props) => { // props.product is the product we are selling.
-  const product = props.product
+  const product = props.product;
+  const cart = useContext(CartContext);
+  const productQuantity = cart.getProductQuantity(product.id);
+
   return (
     <Card>
       <Card.Body>
