542d171b623f6d0259afa99088d14245059fdab6
Thein Naing <theinnaing0908@gmail.com>
2023-06-11 14:20:59 +0800

    also create Sunglesses/Camera product , copy API produt keys in save in server.js for a while.

--- server.js
+++ server.js
@@ -1,2 +1,4 @@
 //pk_test_51NESrqHpTTELhGYNLcO9J2PzPFHJE2mJLBVUuqv5NBF12heMthn2nQDE4Vk6eHJ1jfCepUisyvtNVpGeftS4qosh00zBENIGIi
 //coffee : price_1NHhY9HpTTELhGYNqJko5s5l
+//Sunglesses : price_1NHhbqHpTTELhGYNGYTVa2DF
+//Camera :price_1NHhd2HpTTELhGYNWOiZig6S
