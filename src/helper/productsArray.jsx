3ec2c6a6b395bc135d9fe921c569f62476282624
Thein Naing <theinnaing0908@gmail.com>
2023-06-11 14:58:18 +0800

    start the server. repalce  product id in productsArray with stripe.com product price_id earlier saved

--- src/helper/productsArray.jsx
+++ src/helper/productsArray.jsx
@@ -1,17 +1,19 @@
-
+//coffee : price_1NHhY9HpTTELhGYNqJko5s5l
+//Sunglesses : price_1NHhbqHpTTELhGYNGYTVa2DF
+//Camera :price_1NHhd2HpTTELhGYNWOiZig6S
 const productsArray = [
   {
-      id: "price_1LnUTFDM1jwCEz8OGoOSXiSM",
+      id: " price_1NHhY9HpTTELhGYNqJko5s5l",
       title: "Coffee",
       price: 4.99
   },
   {
-      id: "price_1LnUTxDM1jwCEz8OAqHYTwKQ",
+      id: "price_1NHhbqHpTTELhGYNGYTVa2DF",
       title: "Sunglasses",
       price: 9.99
   },
   {
-      id: "price_1LnUUoDM1jwCEz8OvxIcJ7to",
+      id: "price_1NHhd2HpTTELhGYNWOiZig6S",
       title: "Camera",
       price: 39.99
   }
