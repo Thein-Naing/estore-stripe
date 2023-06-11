9e072a1c6feb3ab2729836e25dbe57d114342fb1
Thein Naing <theinnaing0908@gmail.com>
2023-06-11 15:11:38 +0800

    update server.js

--- server.js
+++ server.js
@@ -3,58 +3,55 @@
 //Sunglesses : price_1NHhbqHpTTELhGYNGYTVa2DF
 //Camera :price_1NHhd2HpTTELhGYNWOiZig6S
 
-//import all dependencies.
 const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const STRIPE_URI='pk_test_51NESrqHpTTELhGYNLcO9J2PzPFHJE2mJLBVUuqv5NBF12heMthn2nQDE4Vk6eHJ1jfCepUisyvtNVpGeftS4qosh00zBENIGIi'
-const PORT = process.env.PORT || 4000
-
-const stripe = require('stripe')(STRIPE_URI);
+var cors = require('cors');
+const stripe = require('stripe')('STRIPR_URI');
 
 const app = express();
-
-app.use(express.static('public'));
 app.use(cors());
+app.use(express.static("public"));
 app.use(express.json());
 
+app.post("/checkout", async (req, res) => {
+    /*
+    req.body.items
+    [
+        {
+            id: 1,
+            quantity: 3
+        }
+    ]
 
-app.post('/checkout', async(req, res)=> {
-  /*
-  req.body.items [
-    {
-      id: 1,
-      quantity: 3
-    }
-  ]
-  but stripe wants like this [
-    {
-      price: 1,
-      quantity: 3
-    }
-  ]
-  */
-  console.log(req.body);
-  const items = req.body.items;
-  let lineItems = [];
-  items.forEach((item)=> {
-    lineItems.push({
-      price: item.id,
-      quantity: item.quantity
-    })
-  })
+    stripe wants
+    [
+        {
+            price: 1,
+            quantity: 3
+        }
+    ]
+    */
+    console.log(req.body);
+    const items = req.body.items;
+    let lineItems = [];
+    items.forEach((item)=> {
+        lineItems.push(
+            {
+                price: item.id,
+                quantity: item.quantity
+            }
+        )
+    });
 
-const session = await stripe.checkout.sessions.create({
-  line_items: lineItems,
-  mode: 'payment',
-  success_url:'http://localhost:3000/success',
-  cancel_url:'http://localhost:3000/cancel',
+    const session = await stripe.checkout.sessions.create({
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: "http://localhost:3000/success",
+        cancel_url: "http://localhost:3000/cancel"
+    });
 
+    res.send(JSON.stringify({
+        url: session.url
+    }));
 });
- res.send(JSON.stringify({
-  url: session.url
- }));
-});
 
-
-app.listen(PORT, ()=> console.log(`Listening on port ${PORT} `));
+app.listen(4000, () => console.log("Listening on port 4000!"));
