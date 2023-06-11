8a4d76c83bd8fbd4e8073a65c96a50ef0e9ee927
Thein Naing <theinnaing0908@gmail.com>
2023-06-11 10:49:24 +0800

    we will create deleteFromCart function before removeFromCart function to more clear understanding. use filter method like below:   const deleteFromCart = (id) => { // filter: [] if an obj meet a condition , then add obj to []. // [p1, p2, p3] => [p1, p3] if there is no id for product 2.    setCartProducts(cartProducts =>     cartProducts.filter(currentProduct => {       return currentProduct.id !== id;     })     )   };

--- src/helper/CartContext.jsx
+++ src/helper/CartContext.jsx
@@ -11,71 +11,71 @@ export const CartContext = createContext({
 });
 
 export const CartProvider = ({ children }) => {
-  const[cartProducts, setCartProducts] = useStae([]);
+  const [cartProducts, setCartProducts] = useStae([]);
   //{id: 1, quantity: 2}
 
-  const getProductQuantity = (id) => { // find total product quantities first.
+  const getProductQuantity = (id) => {
+    // find total product quantities first.
 
-    const quantity = cartProducts.find(product => product.id === id)?.quantity;
-      if (quantity === undefined ) {
-        return 0;
-      }
-      return quantity;
-};
-
-  const addOneToCart = (id) => { // add new one to cart.
-    const quantity = getProductQuantity(id);
-    if (quantity === 0) { //product is not in the cart.
-      setCartProducts(
-        [...cartProducts, { id: id, quantity: 1}]
-      )
-    } else { //product is in the cart.
-    //[...{id: 1, quantity: 3}, { id: 2 , quantity: 1 }] , we wanna add product id: 2.
-      setCartProducts(
-        cartProducts.map( product=>
-          product.id === id ? // if condition
-          {...product, qauntity: product.quantity + 1} // if statement is true.
-          : product // if statement is false.
-        )
-      )
+    const quantity = cartProducts.find(
+      (product) => product.id === id
+    )?.quantity;
+    if (quantity === undefined) {
+      return 0;
     }
+    return quantity;
+  };
 
+  const addOneToCart = (id) => {
+    // add new one to cart.
+    const quantity = getProductQuantity(id);
+    if (quantity === 0) {
+      //product is not in the cart.
+      setCartProducts([...cartProducts, { id: id, quantity: 1 }]);
+    } else {
+      //product is in the cart.
+      //[...{id: 1, quantity: 3}, { id: 2 , quantity: 1 }] , we wanna add product id: 2.
+      setCartProducts(
+        cartProducts.map(
+          (product) =>
+            product.id === id // if condition
+              ? { ...product, qauntity: product.quantity + 1 } // if statement is true.
+              : product // if statement is false.
+        )
+      );
+    }
+  };
+
+
+  const deleteFromCart = (id) => {
+// filter: [] if an obj meet a condition , then add obj to [].
+// [p1, p2, p3] => [p1, p3] if there is no id for product 2.
+   setCartProducts(cartProducts =>
+    cartProducts.filter(currentProduct => {
+      return currentProduct.id !== id;
+    })
+    )
   };
 
 
 
-  const removeOneFromCart = () => {
 
-  };
-
-  const deleteFromCart = () => {
-
-  };
-
-  const getTotalCost = () => {
-
-  };
+  const removeOneFromCart = () => {};
 
 
 
-
-
+  const getTotalCost = () => {};
 
   const contextValue = {
-  items: cartProducts,
-  getProductQuantity,
-  addOneToCart ,
-  removeOneFromCart,
-  deleteFromCart,
-  getTotalCost,
+    items: cartProducts,
+    getProductQuantity,
+    addOneToCart,
+    removeOneFromCart,
+    deleteFromCart,
+    getTotalCost,
+  };
 
-}
-
-return (
-  <CartContext.Provider
-  value={contextValue}
-
-  >{children}</CartContext.Provider>
-);
-
-}
+  return (
+    <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
+  );
+};
