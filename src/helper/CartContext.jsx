f70975024e1aa831624ad8bbdf70734cb4523557
Thein Naing <theinnaing0908@gmail.com>
2023-06-11 10:01:25 +0800

    update CartContext hook with CartProvider function with cartProducts useState hook , calling const contextValue =  {   items: cartProducts,   getProductQuantity,   addOneToCart ,   removeOneFromCart,   deleteFromCart,   getTotalCost,
    } and return :
    return (
      <CartContext.Provider
      value={contextValue}
    
      >{children}</CartContext.Provider>
    );

--- src/helper/CartContext.jsx
+++ src/helper/CartContext.jsx
@@ -1,20 +1,34 @@
 import { createContext, useState } from "react";
 import { productsArray, getProductData } from "./productsStore";
 
-
 export const CartContext = createContext({
-    items: [],
-    getProductQuantity: () => {},
-    addOneToCart: () => {},
-    removeOneFromCart: () => {},
-    deleteFromCart: () => {},
-    getTotalCost: () => {}
+  items: [],
+  getProductQuantity: () => {},
+  addOneToCart: () => {},
+  removeOneFromCart: () => {},
+  deleteFromCart: () => {},
+  getTotalCost: () => {},
 });
 
+export const CartProvider = ({ children }) => {
+  const[cartProducts, setCartProducts] = useStae([]);
+  //{id: 1, quantity: 2}
 
+  const contextValue = {
+  items: cartProducts,
+  getProductQuantity,
+  addOneToCart ,
+  removeOneFromCart,
+  deleteFromCart,
+  getTotalCost,
 
-export const  CartProvider = ({children}) => {
-  <CartProvider>
-    ABC
-  </CartProvider>
+}
+
+return (
+  <CartContext.Provider
+  value={contextValue}
+
+  >{children}</CartContext.Provider>
+);
+
 }
